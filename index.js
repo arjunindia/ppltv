0087bf0e66069eda1b680889a4c769cbb9dbb1f0
Arjun <arjun8107@gmail.com>
2024-07-07 23:12:27 +0530

    l

---
+++ index.js
@@ -0,0 +1,12 @@
+localStorage.setItem(
+  "settings.txt",
+  "BAcAAAAAAAAACgAAAGludHJvX3NlZW4BARAAAABtb3ZlX2Rvd25fYnV0dG9uAgMAAAAAAAAAEAAAAG1vdmVfbGVmdF9idXR0b24CAAAAAAAAAAARAAAAbW92ZV9yaWdodF9idXR0b24CAQAAAAAAAAAOAAAAbW92ZV91cF9idXR0b24CAgAAAAAAAAASAAAAcmVuZGVyaW5nX3NldHRpbmdzAggAAAAAAAAADQAAAHR1dG9yaWFsX3NlZW4BAQ==\u0000",
+);
+localStorage.setItem("settings.txt.hash", "sQCDkiENqdhHsFKz/DCFMw==\u0000");
+
+if (localStorage.getItem("refresh") === "true") {
+  localStorage.setItem("refresh", "false");
+} else {
+  localStorage.setItem("refresh", "true");
+  window.location.reload();
+}
